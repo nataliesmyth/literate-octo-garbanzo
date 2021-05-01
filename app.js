7591128a04f3e617421accbef00cb0160b5e0607
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-01 12:47:07 -0400

    boardrep appears in console

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('testing....')
+// console.log('testing....')
+
+// Constants
+
+// App State (variables)
+    // in order to make a mark, we need a place for the mark to be made
+    // makiing a mark means the game is initiated
+let board;
+
+// Cached Element References
+
+// Event Listeners
+
+// Functions
+function init() {
+    board = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
+    render();
+};
+
+init();
+
+// making a mark renders a change in the game
+function render() {
+    board.forEach(function(mark, index){
+        console.log(mark, index);
+    });
+};
+
