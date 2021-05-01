91867c7f896621be4e7ffa1d0cbe6560448e23d6
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-01 13:47:59 -0400

    one mark made fully

--- app.js
+++ app.js
@@ -8,7 +8,9 @@
 let board;
 
 // Cached Element References
-
+    // grab squares
+const squares = Array.from(document.querySelectorAll('#board div'));
+    // the array.from() function makes an array from all elements returned by querySelectorAll. querySelectorAll finds the elements with the id of .boardand selects all the div children of that element. This way, we didn't have to give each square an id, select them individually and build a new array.
 // Event Listeners
 
 // Functions
@@ -27,6 +29,8 @@ init();
 function render() {
     board.forEach(function(mark, index){
         console.log(mark, index);
+        // set text content of square of the same position to the mark on the board
+        squares[index].textContent = mark;
     });
 };
 
