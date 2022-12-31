de2fe3ea5736e26a88ca1a2a0735eb0e74613696
Nataliesmyth <nataliesmyth11@gmail.com>
2022-12-31 14:14:26 -0500

    updated game

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// console.log('testing....')
-
-/****** CONSTANTS ******/
-
+const xClass = 'x';
+const oClass = 'o'
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,88 +10,86 @@ const winningCombos = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+const cellEls = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+let oTurn; // this will determine whose turn it is
+winningMsgTxtEl = document.querySelector('[data-winning-msg-txt]');
+winningMsgEl = document.getElementById('winningMsg')
+const restartBtn = document.getElementById('restartBtn');
+startGame();
 
-/****** APP STATE (variables) ******/
-
-let board;
-
-let turn = 'X'
-
-let win;
-
-/****** CACHED ELEMENT REFERENCES ******/
-
-// grab squares
-const squares = Array.from(document.querySelectorAll('#board div'));
-// the array.from() function makes an array from all elements returned by querySelectorAll. querySelectorAll finds the elements with the id of .boardand selects all the div children of that element. This way, we didn't have to give each square an id, select them individually and build a new array.
-
-const messages = document.querySelector('h2');
-
-/****** EVENT LISTENERS ******/
-
-document.getElementById('board').addEventListener('click', handleTurn);
-document.getElementById('reset-button').addEventListener('click', init);
-
-/****** FUNCTIONS ******/
-
-function handleTurn(event) {
-    let idx = squares.findIndex(function(square) {
-        return square === event.target;
+restartBtn.addEventListener('click', startGame);
+function startGame() {
+    oTurn = false;
+    cellEls.forEach(cell => {
+        // remove x's and o's from board
+        cell.classList.remove(xClass)
+        cell.classList.remove(oClass)
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true }); // the once property only allows the event listener to fire once when set to true
     });
-    board[idx] = turn;
-    // console.log(board);
-    // ternary statement for X or O
-    // logic: if it is X's turn, assign the turn to O; if it's not X's turn, assign the turn to X
-    // <condition> ? <if condition is true, this> : <else if condition is false, this>
-    turn = turn === 'X' ? '0' : 'X';
-    // console.log(turn);
-    // console.log(win)
-    win = getWinner();
-    console.log(win);
-    render();
-    // win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null ;
+
+    setBoardHoverClass();
+    // remove popup message
+    winningMsgEl.classList.remove('show')
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? oClass : xClass;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+        // console.log('winner')
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        // we only want to switch turns if there is no winner yet
+        switchTurn()
+        setBoardHoverClass()
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMsgTxtEl.innerText = 'Draw!'
+    } else {
+        console.log('winner')
+        winningMsgTxtEl.innerText = `${oTurn ? "O's" : "X's"} Wins!`;
+    }
+    winningMsgEl.classList.add('show');
+}
+
+function isDraw() {
+    return [...cellEls].every(cell => {
+        return cell.classList.contains(xClass) || cell.classList.contains(oClass)
+    })
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function switchTurn() {
+    // Whenever this function is invoked, the turn will equal the opposite turn
+    oTurn = !oTurn
 };
 
-function init() {
-    board = [
-        '', '', '',
-        '', '', '',
-        '', '', ''
-    ];
-    render();
-};
+function setBoardHoverClass() {
+    board.classList.remove(xClass);
+    board.classList.remove(oClass);
+    if (oTurn) {
+        board.classList.add(oClass);
+    } else {
+        board.classList.add(xClass)
+    }
+}
 
-init();
-
-// making a mark renders a change in the game
-function render() {
-    board.forEach(function(mark, index){
-        // console.log(mark, index);
-        // set text content of square of the same position to the mark on the board
-        squares[index].textContent = mark;
-    });
-    if ( win === 'T' ) {
-        messages.textContent = `That's a tie!`
-      } else if (win) { 
-        messages.textContent = `${win} wins the game!`
-      } else {
-        messages.textContent = `It's ${turn}'s turn!`
-      }
-    // messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
-};
-
-function getWinner() {
-    let winner = null;
-    
-    winningCombos.forEach((combo, index) => {
-        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
-            winner = board[combo[0]];
-        } 
-    });
-    
-    return winner ? winner : board.includes('') ? null : 'T';
-    
-};
-
-
-console.log(getWinner);
+function checkWin(currentClass) {
+    // if the current class is in all 3 of the elements inside the combination, user is a winner
+    return winningCombos.some(combinations => {
+        return combinations.every(index => {
+            return cellEls[index].classList.contains(currentClass)
+        })
+    })
+}
